2e87e4664e59666cc06cbf719a95e2a76d47c1b2
standy <mostovoyav@gmail.com>
2014-10-11 14:41:37 +0400

    file size

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,26 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var size = require('gulp-size');
 
 
-gulp.task('default', function() {
+gulp.task('make', function() {
+
     return gulp.src(['./jquery.custom-scroll.js'])
-        .pipe(uglify())
+        .pipe(uglify({
+            preserveComments: function(node, comment) {
+                return comment.pos === 0;
+            }
+        }))
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./'));
 });
 
+
+gulp.task('default', ['make'], function() {
+    return gulp.src(['./jquery.custom-scroll.css', './jquery.custom-scroll.js', './jquery.custom-scroll.min.js', ])
+        .pipe(size({showFiles: true, gzip: true}))
+});
 
 gulp.task('watch', function() {
     gulp.watch('./jquery.custom-scroll.js', ['default']);
