2ece23edabda902092684b574b7844279a0d9495
standy <mostovoyav@gmail.com>
2014-11-15 14:32:30 +0300

    ver up

--- jquery.custom-scroll.js
+++ jquery.custom-scroll.js
@@ -1,4 +1,4 @@
-/* jQuery Custom Scroll plugin v0.6.4 | (c) 2014 Mostovoy Andrey | https://github.com/standy/custom-scroll/blob/master/LICENSE */
+/* jQuery Custom Scroll plugin v0.6.5 | (c) 2014 Mostovoy Andrey | https://github.com/standy/custom-scroll/blob/master/LICENSE */
 (function($) {
 	$.fn.customScroll = function(options) {
 		if (!this.length) {
